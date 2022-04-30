6412230cc66fa201e83e59afe91056447982613f
Gabriel Machado <gabriel@in-vert.io>
2022-04-29 23:59:56 -0300

    Configuration and setup of the project

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Container fluid>
+        <Row>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
